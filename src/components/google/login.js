13879a5b762fe1c24667da75de4ccf1f2f1428ae
TWC58 <47580948+TWC58@users.noreply.github.com>
2022-04-20 16:28:12 -0400

    heroku deploy

--- src/components/google/login.js
+++ src/components/google/login.js
@@ -5,14 +5,14 @@ import { Button } from '@mui/material';
 
 const clientId = "540240407763-v90k1276kl5v93s6hu5jfc8n42vk1c5b.apps.googleusercontent.com";
 const clientSecret = "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";
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:5000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 
 function Login() {
 
     const { auth } = useContext(AuthContext);
 
     const handleLogin = async () => {
-        console.log(process.env.BACKEND_URL);
+        console.log(BACKEND_URL);
         const googleLoginURL = BACKEND_URL +'/auth/google';
         const newWindow = window.open(
             googleLoginURL,
